3d90b5e2cc621ba972d0b8187b833c86e180a255
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-06-10 23:50:03 +0530

    Update vectorstore_manager.py

--- rag/vectorstore_manager.py
+++ rag/vectorstore_manager.py
@@ -1,1 +1,28 @@
+import os
 
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+DB_PATH = "vectorstore"
+
+embeddings = HuggingFaceEmbeddings(
+    model_name="all-MiniLM-L6-v2"
+)
+
+
+def load_vectorstore():
+
+    if os.path.exists(DB_PATH):
+
+        return FAISS.load_local(
+            DB_PATH,
+            embeddings,
+            allow_dangerous_deserialization=True
+        )
+
+    return None
+
+
+def save_vectorstore(vs):
+
+    vs.save_local(DB_PATH)
