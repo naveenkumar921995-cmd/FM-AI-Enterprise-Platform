b9c93bb83cb1d29be2af9ee8c60f66a8d37de057
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-06-10 22:49:10 +0530

    Create vendor_service.py

---
+++ database/vendor_service.py
@@ -0,0 +1,24 @@
+vendors = [
+
+    {
+        "vendor":
+            "Carrier",
+
+        "service":
+            "HVAC",
+
+        "amc_expiry":
+            "2027-12-31"
+    },
+
+    {
+        "vendor":
+            "Honeywell",
+
+        "service":
+            "Fire System",
+
+        "amc_expiry":
+            "2026-08-15"
+    }
+]
