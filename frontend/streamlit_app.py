75363620593a7f4fb6c864666431f5c75e0e9da5
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-06-10 23:38:27 +0530

    Update streamlit_app.py

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -162,3 +162,37 @@ elif page == "Incidents":
         incident_data,
         use_container_width=True
     )
+elif page == "Analytics":
+
+    st.title("Facility Analytics")
+
+    df = pd.DataFrame({
+
+        "Severity": [
+            "Low",
+            "Medium",
+            "High",
+            "Critical"
+        ],
+
+        "Count": [
+            12,
+            9,
+            4,
+            2
+        ]
+    })
+
+    st.bar_chart(
+        df.set_index("Severity")
+    )
+
+    st.metric(
+        "MTTR",
+        "3.5 Hours"
+    )
+
+    st.metric(
+        "SLA Compliance",
+        "96%"
+    )
