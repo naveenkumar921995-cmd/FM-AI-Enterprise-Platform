6342583106fe7a5e3e27b5e64e5d3d4b58113871
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-06-10 23:50:30 +0530

    Update uploader.py

--- rag/uploader.py
+++ rag/uploader.py
@@ -1,14 +1,50 @@
-from rag.loader import load_documents
-from rag.chunker import split_documents
-from rag.retriever import create_vectorstore
+from langchain_community.document_loaders import PyPDFLoader
+
+from langchain_text_splitters import (
+    RecursiveCharacterTextSplitter
+)
+
+from langchain_community.vectorstores import FAISS
+
+from rag.vectorstore_manager import (
+    embeddings,
+    load_vectorstore,
+    save_vectorstore
+)
 
 
-def process_new_document():
+def process_pdf(pdf_path):
 
-    docs = load_documents()
+    loader = PyPDFLoader(pdf_path)
 
-    chunks = split_documents(docs)
+    documents = loader.load()
 
-    create_vectorstore(chunks)
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=50
+    )
 
-    return "Knowledge base updated"
+    chunks = splitter.split_documents(
+        documents
+    )
+
+    existing_db = load_vectorstore()
+
+    if existing_db:
+
+        existing_db.add_documents(
+            chunks
+        )
+
+        save_vectorstore(existing_db)
+
+    else:
+
+        db = FAISS.from_documents(
+            chunks,
+            embeddings
+        )
+
+        save_vectorstore(db)
+
+    return len(chunks)
