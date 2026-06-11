4ac24da1b3f7b34a2fc607434edf253f43ae12d3
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-06-11 21:24:35 +0530

    Create dashboard_service.py

---
+++ database/dashboard_service.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+
+def get_dashboard_metrics():
+
+    return {
+
+        "sla": 97.2,
+
+        "mttr": 1.9,
+
+        "mtbf": 245,
+
+        "asset_availability": 99.1,
+
+        "open_incidents": 7,
+
+        "closed_incidents": 17,
+
+        "open_workorders": 210,
+
+        "critical_workorders": 12,
+
+        "energy_reduction": 18,
+
+        "occupant_satisfaction": 94
+    }
